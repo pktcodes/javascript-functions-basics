eaaf1c58fa52700a7ad5451e5cb54f87a3de4d63
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-19 22:18:10 -0500

    Closure - Complete Example

--- app.js
+++ app.js
@@ -4,16 +4,45 @@
 
 function newAccount(name, initialDeposit) {
   let balance = initialDeposit;
+
   function showBalance() {
     console.log(`Hey ${name}, your balance is ${balance}`);
   }
-  return showBalance;
-}
 
-newAccount("susan", 500)();
+  function deposit(amount) {
+    balance += amount;
+    showBalance();
+  }
+
+  function withdraw(amount) {
+    if (amount > balance) {
+      console.log(`Hey ${name}, you don't have enough funds`);
+      return;
+    }
+    balance -= amount;
+    showBalance();
+  }
+
+  return { showBalance: showBalance, deposit: deposit, withdraw: withdraw };
+}
 
 const john = newAccount("john", 300);
 const bob = newAccount("bob", 1000);
 
-john();
-bob();
+console.log(john);
+
+john.showBalance();
+john.deposit(200);
+john.deposit(1000);
+
+/* Private Variable */
+
+// Trying to get john balance and set it, which is not possible. we don't have access to balance since we are not returning the balance which is a feature of "CLOSURE"
+
+john.balance = 10000;
+// console.log("John's Balance : ", john.balance);
+// console.log("John: ", john);
+
+john.withdraw(2000);
+
+bob.showBalance();
